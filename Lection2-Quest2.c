3b53e2c06f6f9c72a3b7ee5773cb343ad8207676
moonshy <vladbobkb10@gmail.com>
2026-02-10 21:40:14 +0300

    Lection2-Quest2

---
+++ Lection2-Quest2.c
@@ -0,0 +1,11 @@
+// Переименовать в named_hello.c. Работать в ветке develop
+
+#include <stdio.h>
+
+int main() {
+    int number;
+    scanf("%d", &number);
+    printf("Hello, %d!", number);
+
+    return 0;
+}
